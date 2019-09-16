325bd4cbb6aff557cf540aaf084693855be82952
ICE <ice@lab04m22.nrc.ice.ufjf.br>
2019-09-16 20:58:06 -0300

    Criando o jogo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ZombieAttack
+# Zombie Attack
 Repositório para Trabalho Avaliativo (Game Zombie Attack)
+Jogo Básico Realizado com JavaScript e Canvas
---
+++ Scene.js
@@ -0,0 +1,149 @@
+function Scene(params) {
+    var exemplo = {
+        sprites: [],
+        toRemove: [],
+        ctx: null,
+        w: 300,
+        h: 300,
+        pontos: 0,
+        contador: 0,
+        fase:1
+    }
+
+    Object.assign(this, exemplo, params);
+}
+
+Scene.prototype = new Scene();
+Scene.prototype.constructor = Scene;
+
+Scene.prototype.adicionar = function (sprite) {
+    this.sprites.push(sprite);
+    sprite.scene = this;
+}
+
+Scene.prototype.desenhaCenario = function () {
+    for (var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].desenhaTela(this.ctx);
+    }
+}
+
+Scene.prototype.desenhar = function () {
+    for (var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].desenhar(this.ctx);
+    }
+}
+
+Scene.prototype.mover = function (dt) {
+    for (var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].mover(dt);
+    }
+}
+
+Scene.prototype.comportar = function () {
+    for (var i = 0; i < this.sprites.length; i++) {
+        if (this.sprites[i].comportar) {
+            this.sprites[i].comportar();
+        }
+    }
+}
+
+Scene.prototype.limpar = function () {
+    this.ctx.clearRect(0, 0, this.w, this.h);
+}
+
+Scene.prototype.removeSprites = function () {
+    for (var i = 0; i < this.toRemove.length; i++) {
+        var idx = this.sprites.indexOf(this.toRemove[i]);
+        if (idx >= 0) {
+            this.sprites.splice(idx, 1);
+        }
+    }
+    this.toRemove = [];
+};
+
+Scene.prototype.checaColisao = function(){
+    for(var i = 0; i<this.sprites.length; i++){
+        for(var j = i+1; j<this.sprites.length; j++){
+            if(this.sprites[i].colidiuCom(this.sprites[j])){
+                if(this.sprites[i].props.tipo === "npc" && this.sprites[j].props.tipo ==="tiro"){
+                    this.toRemove.push(this.sprites[i]);
+                    this.toRemove.push(this.sprites[j]);
+                    this.pontos++;
+                }
+                
+                if(this.sprites[i].props.tipo === "torre" && this.sprites[j].props.tipo ==="npc"){
+                    this.fase = 0;
+                }
+
+                if(this.sprites[i].props.tipo === "torre" && this.sprites[j].props.tipo ==="nemesis"){
+                    this.fase = 0;
+                }
+            }
+        }
+    }  
+};
+
+Scene.prototype.checaColisaoNemesis = function(){
+    for(var i = 0; i<this.sprites.length; i++){
+        for(var j = i+1; j<this.sprites.length; j++){
+            if(this.sprites[i].colidiuCom(this.sprites[j])){
+                if(this.sprites[i].props.tipo === "nemesis" && this.sprites[j].props.tipo ==="tiro"){
+                    this.toRemove.push(this.sprites[j]);
+                    this.contador++;
+
+                    if(this.contador === 4){
+                        this.toRemove.push(this.sprites[i]);
+                        this.toRemove.push(this.sprites[j]);
+                        this.pontos++;
+                        this.contador = 0;
+                    }
+                }
+            }
+        }
+    }  
+};
+
+
+Scene.prototype.pontuacao = function () {
+    for (var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].pontuacao(this.pontos,this.ctx);
+    }
+};
+
+Scene.prototype.gameOver = function () {
+    for (var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].gameOver(this.ctx);
+    }
+};
+
+Scene.prototype.passo = function () {
+    switch (this.fase) {
+        case 1:
+            this.passo1();
+            break;
+    
+        default:this.fim();
+            break;
+    }
+}
+
+Scene.prototype.passo1 = function () {
+    this.limpar();
+    this.desenhaCenario();
+    this.comportar();
+    this.mover(dt);
+    this.desenhar();
+    this.checaColisao();
+    this.checaColisaoNemesis();
+    this.pontuacao();
+    this.removeSprites();
+}
+
+Scene.prototype.fim = function () {
+    this.limpar();
+    this.desenhaCenario();
+    this.desenhar();
+    this.gameOver();
+    this.pontuacao();
+    
+}
